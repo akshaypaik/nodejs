1afcecb56ff8a2dc7ae17eb1f94972cf256d2b07
Akshay Pai <akshaypaik@gmail.com>
2025-06-08 18:57:05 +0530

    [INTRODUCTION]: event loop code snippet

---
+++ Introduction/event loop.js
@@ -0,0 +1,25 @@
+import fs from "fs";
+// const fs = require("fs");
+
+console.log("start");
+
+setImmediate(() => console.log("setImmediate"));
+
+process.nextTick(() => {
+    process.nextTick(() => console.log("nextTick two"));
+    console.log("nexTick one");
+});
+
+// In esm module promise is priority over nextTick.
+// In commonjs module nextTick is priority over promise
+const promise = new Promise(resolve => resolve("promise"));
+
+setTimeout(() => console.log("timer one"), 0);
+
+fs.readFile("./file.txt", "utf8", () => {
+    console.log("file read");
+});
+
+promise.then(res => console.log(res));
+
+console.log("end");
