fff39e0922a075a303b3c4bc4eb2c8e05db9a414
Akshay Pai <akshaypaik@gmail.com>
2025-06-09 15:57:43 +0530

    [INTRODUCTION]: thread pool of libuv

--- Introduction/thread pool.js
+++ Introduction/thread pool.js
@@ -6,35 +6,27 @@
 // 4. user specified input
 
 // import fs from "fs";
-import crypto from "crypto";
+
 
 // const crypto = require("crypto");   // commonjs 
 
 // [IMPORTANT]
 // process.env.UV_THREADPOOL_SIZE = 8; // we can alter the UV Thread Pool size like this which overrides the defualt number of 4.
 
-// one thread pool is assigned to this
-crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
-    console.log("1 - cryptoPBKDF2 done");
-});
+import crypto from "crypto";
 
 // one thread pool is assigned to this
-crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
-    console.log("2 - cryptoPBKDF2 done");
-});
+crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {console.log("1 - cryptoPBKDF2 done");});
 
 // one thread pool is assigned to this
-crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
-    console.log("3 - cryptoPBKDF2 done");
-});
+crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {console.log("2 - cryptoPBKDF2 done");});
 
 // one thread pool is assigned to this
-crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
-    console.log("4 - cryptoPBKDF2 done");
-});
+crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {console.log("3 - cryptoPBKDF2 done");});
+
+// one thread pool is assigned to this
+crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {console.log("4 - cryptoPBKDF2 done");});
 
 // this exceeds the maximum thread pool of 4. so this will be picked up when one of the above four is done.
-crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
-    console.log("5 - cryptoPBKDF2 done");
-});
+crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {console.log("5 - cryptoPBKDF2 done");});
 
